2dbf121e1884f5ad84f7725383bf3afe39f35b1c
Ana Vázquez <ana.maria.vl@hotmail.com>
2019-03-21 13:06:25 -0600

    Se agrega dashboard y HTTP

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,13 +2,16 @@ import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'src/hero_service.dart';
 import 'src/hero_list_component.dart';
-import 'src/routes.dart';
+import 'src/route.dart';
 
 @Component(
   selector: 'my-app',
+  styleUrls: ['app_component.css'],
   template: '''
   <h1>{{title}}</h1>
   <nav>
+    <a [routerLink]="RoutePaths.dashboard.toUrl()"
+       [routerLinkActive]="'active'">Dashboard</a>
     <a [routerLink]="RoutePaths.heroes.toUrl()"
        [routerLinkActive]="'active'">Heroes</a>
   </nav>
@@ -18,6 +21,7 @@ import 'src/routes.dart';
   providers: [ClassProvider(HeroService)],
   exports: [RoutePaths, Routes],
 )
+
 class AppComponent {
   final title = 'Tour of Heroes';
 }
